e7cfcd3e51f6633dd983667620e9a04fdbbe1fe0
6192836CEE <128358992+6192836CEE@users.noreply.github.com>
2023-05-05 16:56:41 +0200

    dynamic meta tags test

--- index.js
+++ index.js
@@ -2,3 +2,34 @@
 import './style.css';
 
 // Write Javascript code!
+setTimeout(() => {
+  document.querySelector('head').innerHTML =
+    document.querySelector('head').innerHTML +
+    `
+  
+  <title>Title</title>
+
+    <title>Title</title>
+    <meta name="title" content="Title" />
+    <meta name="description" content="Description" />
+
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://metatags.io/" />
+    <meta property="og:title" content="Title" />
+    <meta property="og:description" content="Description" />
+    <meta
+      property="og:image"
+      content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+    />
+
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://metatags.io/" />
+    <meta property="twitter:title" content="Title" />
+    <meta property="twitter:description" content="Description" />
+    <meta
+      property="twitter:image"
+      content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+    />
+  
+  `;
+}, 200);
