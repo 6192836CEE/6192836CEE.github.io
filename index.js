d022aa89b62306e9df72bee8c138d99502bf2f4c
6192836CEE <128358992+6192836CEE@users.noreply.github.com>
2023-05-05 17:02:10 +0200

    overwrite existing ones

--- index.js
+++ index.js
@@ -3,20 +3,15 @@ import './style.css';
 
 // Write Javascript code!
 // setTimeout(() => {
-document.querySelector('head').innerHTML =
-  document.querySelector('head').innerHTML +
-  `
-  
-  <title>Title</title>
-
+document.querySelector('head').innerHTML = `
     <title>Title</title>
-    <meta name="title" content="Title" />
-    <meta name="description" content="Description" />
+    <meta name="title" content="TitleJS" />
+    <meta name="description" content="DescriptionJS" />
 
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://metatags.io/" />
-    <meta property="og:title" content="Title" />
-    <meta property="og:description" content="Description" />
+    <meta property="og:title" content="TitleJS" />
+    <meta property="og:description" content="DescriptionJS" />
     <meta
       property="og:image"
       content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
@@ -24,8 +19,8 @@ document.querySelector('head').innerHTML =
 
     <meta property="twitter:card" content="summary_large_image" />
     <meta property="twitter:url" content="https://metatags.io/" />
-    <meta property="twitter:title" content="Title" />
-    <meta property="twitter:description" content="Description" />
+    <meta property="twitter:title" content="TitleJS" />
+    <meta property="twitter:description" content="DescriptionJS" />
     <meta
       property="twitter:image"
       content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
